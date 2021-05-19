7aae52c640eaa5d93d7ef1b8f6165ef8680d82d6
barisuygun <buygun75@gmail.com>
2021-05-19 14:47:43 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="navbar">
+    <div class="navbar-item navbar-brand"><a class="navbar-link" href="">Problog</a></div>
+    <ul class="navbar-list">
+        <li class="navbar-item"><a class="navbar-link navbar-links" href="">Home</a></li>
+        <li class="navbar-item"><a class="navbar-link navbar-links" href="">Posts</a></li>
+        <li class="navbar-item"><a class="navbar-link navbar-links" href="">About</a></li>
+    </ul>
+    <div class="navbar-item">
+        <div class="navbar-link navbar-hamburger"><i class="fas fa-bars"></i></div>
+    </div>
+</div>
+
+<div class="navbar-mobile">
+    <div class="navbar-item navbar-brand"><a class="navbar-link" href="">Problog</a></div>
+    <div class="navbar-item">
+        <div class="navbar-link navbar-hamburger"><i class="fas fa-bars"></i></div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problog</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Goblin+One&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/index.css" />
+
+</head>
+
+<body>
+    <?php require "header.php" ?>
+    <div class="content-wrap">
+
+        <div class="left-wrap">
+            <h1 class="welcome-text">Welcome to Problog</h1>
+            <br>
+            <p class="welcome-info">Here you can share anything you love with anyone !</p>
+            <br>
+            <button class="btn">See Posts</button>
+            <br>
+            <h2 style="margin-left:40%;width:300px;text-align: center;" class="btn">Our Curators</h2>
+            <div class="cards">
+                <div class="card fill-purple" data-card="0">
+                    <div class="card__icon" data-icon="B.U."></div>
+                    <div class="card__detail">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
+                </div>
+                <div class="card fill-blue" data-card="1">
+                    <div class="card__icon" data-icon="S.S."></div>
+                    <div class="card__detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. </div>
+                </div>
+                <div class="card fill-green" data-card="2">
+                    <div class="card__icon" data-icon="D.Y."></div>
+                    <div class="card__detail">Lorem ipsum dolor sit.</div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="bubble bubble--8"></div>
+    <div class="bubble bubble--9"></div>
+    <div class="bubble bubble--10"></div>
+    <div class="bubble bubble--11"></div>
+    <div class="bubble bubble--12"></div>
+    <div class="bubble bubble--1"></div>
+    <div class="bubble bubble--2"></div>
+    <div class="bubble bubble--3"></div>
+    <div class="bubble bubble--4"></div>
+    <div class="bubble bubble--5"></div>
+    <div class="bubble bubble--6"></div>
+    <div class="bubble bubble--7"></div>
+
+    <img class="rightring" src="./images/sttr.png">
+
+
+    <script src="./js/index.js"></script>
+</body>
+
+</html>
